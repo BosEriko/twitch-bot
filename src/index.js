57b8fc6ed4633d603200a1e7af62ef423c36978c
Bos Eriko Reyes <bos.eriko@gmail.com>
2021-05-16 18:25:32 +0800

    tae change

--- src/index.js
+++ src/index.js
@@ -29,19 +29,33 @@ function onMessageHandler (target, context, msg, self) {
 
   // If the command is known, let's execute it
   if (commandName === '!dice') {
-    const num = rollDice();
+    const num = roll(6);
     client.say(target, `You rolled a ${num}`);
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
+  
+  // If the command is known, let's execute it
+  if (commandName === '!pogirate') {
+    const num = roll(10);
+    client.say(target, `You are ${num} of 10 pogi.`);
+    console.log(`* Executed ${commandName} command`);
+  }
+  
+   // If the command is known, let's execute it
+  if (commandName === '!gandarate') {
+    const num = roll(10);
+    client.say(target, `You are ${num} of 10 ganda.`);
+    console.log(`* Executed ${commandName} command`);
+  }
+  
   if (commandName === 'F') {
     client.say(target, `- Fs in the chat bois.`);
   }
 }
 // Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
+function roll(sides) {
   return Math.floor(Math.random() * sides) + 1;
 }
 // Called every time the bot connects to Twitch chat
