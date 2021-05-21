34c55ad41feb2657c2aaf6189a1c1c5d2b57ca3f
BosEriko <bos.eriko@gmail.com>
2021-05-21 22:38:06 +0800

    remove async/await

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ const opts = {
 const client = new tmi.client(opts);
 
 // Fetch API
-let getChannelFunction = async (userID) => {
-  await axios.get(`https://api.twitch.tv/kraken/channels/${userID}/videos`, {
+let getChannelFunction = (userID) => {
+  axios.get(`https://api.twitch.tv/kraken/channels/${userID}/videos`, {
     method: 'GET',
     mode: 'cors',
     cache: 'no-cache',
